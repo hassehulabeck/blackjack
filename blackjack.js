b3f6605ac890e627549dab1b3f13d97380d82f4e
hassehulabeck <hansbudandersson@gmail.com>
2019-10-10 08:55:30 +0200

    minor fixes

--- blackjack.js
+++ blackjack.js
@@ -3,7 +3,8 @@ var suits = ["hearts", "clubs", "diamonds", "spades"];
 var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var dealer = {
     hand: [],
-    isActive: true,
+    canContinue: true,
+    isBusted: false,
     total: null
 }
 var players = [];
@@ -29,11 +30,16 @@ function shuffleArray(array) {
 }
 
 function createPlayers(antal) {
+    let names = ["Ali", "Bashir", "Claudia", "Des", "Erica", "Filip", "Gunnar"];
+
+    // Placera dealern som player 0.
+    players.push(dealer);
+
     for (let i = 0; i < antal; i++) {
         players.push({
             hand: [],
-            name: "player" + i,
-            isActive: true,
+            name: names[Math.floor(Math.random() * names.length)],
+            isBusted: false,
             total: null
         })
     }
@@ -78,13 +84,98 @@ function sumCards() {
             } else {
                 player.total += kort.rank;
             }
-            // Men vad händer om vi har flera äss?
         })
     })
 }
 
+function isPlayerBusted() {
+    // Har spelaren fått mer än 21?
+    players.forEach(function (player) {
+        if (player.total > 21) {
+            player.isBusted = true;
+        }
+    })
+}
+
+function status() {
+    // Visa spelarnas kortsummma.
+    players.forEach(function (player, index) {
+        if (index == 0) {
+            // Visa endast dealerns andra kort (det första är dolt för spänningens skull, samt för att göra det svårare för spelarna.)
+            console.log("Dealer: " + player.hand[1].rank);
+        } else {
+            // Alla andra är spelare.
+            if (!player.isBusted)
+                console.log(player.name + ": " + player.total)
+            else
+                console.log(player.name + ": " + player.total + ' BUSTED!')
+        }
+    })
+
+}
+
+
+function hitMe(player) {
+    // En funktion för att ta ett kort till. Anropas med indexnummer.
+    players[player].hand.push(deck.pop());
+    sumCards();
+}
+
+function dealerCount() {
+    dealer.total = null;
+    dealer.hand.forEach(function (kort) {
+        // Börja med att kolla totalvärdet, och fortsätt om det är mindre än 17.
+        if (dealer.total < 17) {
+            if ((ranks.indexOf(kort.rank) > 8) &&
+                (ranks.indexOf(kort.rank) < 12)) {
+                // Klädda kort.
+                dealer.total += 10;
+            } else if (ranks.indexOf(kort.rank) == 12) {
+                // Äss.
+                if (dealer.total < 11) {
+                    dealer.total += 11;
+                } else {
+                    dealer.total += 1;
+                }
+            } else {
+                dealer.total += kort.rank;
+            }
+        } else {
+            dealer.canContinue = false;
+        }
+    })
+
+}
+
+function checkResults() {
+    players.forEach(function (player, index) {
+        if (index > 0) {
+            if (player.isBusted) {
+                console.log(player.name + ": Dealer wins")
+            } else {
+                if (player.total > dealer.total) {
+                    console.log(player.name + " har vunnit")
+                } else {
+                    // Dealern vinner både om den har bättre hand och om den har samma hand som spelaren.
+                    console.log("Dealern har vunnit mot " + player.name)
+                }
+            }
+        }
+    })
+}
+
+function revealDealersHand() {
+    // En funktion för att "flippa upp" det dolda kortet hos dealern.
+    console.log("Dealer: " + dealer.hand[0].rank + "+" + dealer.hand[1].rank)
+    console.log(dealer.total)
+    if (!dealer.canContinue) {
+        console.log("Dealer has to stop");
+    }
+}
+
+// Initiera och dela ut två kort till alla.
 createDeck();
 shuffleArray(deck);
-createPlayers(2);
+createPlayers(3);
 dealCards(2);
 sumCards();
